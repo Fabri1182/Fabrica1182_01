d652b082cd94884d9f433b7344534418f8c10caa
Fabri1182 <electronica.deetel@gmail.com>
2025-05-17 06:39:35 +0000

    Actualizo configuraciones

---
+++ setup.sh
@@ -0,0 +1,6 @@
+code --install-extension vscode-icons-team.vscode-icons \
+     --install-extension ecmel.vscode-html-css \
+     --install-extension ms-vscode.vscode-typescript-next \
+     --install-extension ritwickdey.LiveServer \
+     --install-extension ms-azuretools.vscode-docker \
+     --install-extension ms-python.python
