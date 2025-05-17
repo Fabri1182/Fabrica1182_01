3de7d375ee0385e57d5cef53ffd6943828a2d3f5
Fabri1182 <electronica.deetel@gmail.com>
2025-05-17 06:57:47 +0000

    Actualizo extwnsiones

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
-code --install-extension vscode-icons-team.vscode-icons \
-     --install-extension ecmel.vscode-html-css \
-     --install-extension ms-vscode.vscode-typescript-next \
-     --install-extension ritwickdey.LiveServer \
-     --install-extension ms-azuretools.vscode-docker \
-     --install-extension ms-python.python
+code --install-extension Extensiones instaladas en Codespaces: didactic lamp:
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension eamodio.gitlens
+code --install-extension ecmel.vscode-html-css
+code --install-extension github.codespaces
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension github.github-vscode-theme
+code --install-extension github.vscode-pull-request-github
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-ceintl.vscode-language-pack-es
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension ritwickdey.liveserver
+code --install-extension vscode-icons-team.vscode-icons
